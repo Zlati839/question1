bba99a4bd627868d6b29cd07efa87ba0740c45df
Zlati839 <zlatipenev7@gmail.com>
2026-01-15 09:47:59 +0200

    Add functionality to save user selections

--- app.py
+++ app.py
@@ -18,11 +18,14 @@ if "sports" not in st.session_state:
 st.subheader("Избери любими неща")
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+
 if st.button("Запази избора"):
     st.session_state.colors[color] += 1
     st.session_state.sports [sport] += 1
     st.success("Изборът е записан!")
+   
 st.divider()
+
 st.subheader(" Резултати")
 
 st.write("Любими цветове")
